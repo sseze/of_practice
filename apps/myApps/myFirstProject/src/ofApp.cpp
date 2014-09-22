459c9d81e8006498ee0da0f104ec67e236be1147
sseze <sseze.ek@gmail.com>
2014-09-22 15:34:44 +0900

    3つの円をランダムにバウンドさせる

--- apps/myApps/myFirstProject/src/ofApp.cpp
+++ apps/myApps/myFirstProject/src/ofApp.cpp
@@ -1,9 +1,17 @@
 #include "ofApp.h"
 
-float loc_x; // 円のx座標
-float loc_y; // 円のy座標
-float speed_x; // x軸方向のスピード
-float speed_y; // y軸方向のスピード
+float loc_x1; // 円のx座標
+float loc_y1; // 円のy座標
+float speed_x1; // x軸方向のスピード
+float speed_y1; // y軸方向のスピード
+float loc_x2; // 円のx座標
+float loc_y2; // 円のy座標
+float speed_x2; // x軸方向のスピード
+float speed_y2; // y軸方向のスピード
+float loc_x3; // 円のx座標
+float loc_y3; // 円のy座標
+float speed_x3; // x軸方向のスピード
+float speed_y3; // y軸方向のスピード
 
 //--------------------------------------------------------------
 // 初期化関数: プログラム実行時に1度だけ実行される
@@ -17,11 +25,23 @@ void ofApp::setup(){
     ofSetCircleResolution(64); // 円の解像度を設定
     
     // 円の中心座標を初期化
-    loc_x = 0;
-    loc_y = 0;
+    loc_x1 = ofRandom(0, ofGetWidth());
+    loc_y1 = ofRandom(0, ofGetHeight());
     // 初速度を定義
-    speed_x = 4;
-    speed_y = 7;
+    speed_x1 = ofRandom(-10, 10);
+    speed_y1 = ofRandom(-10, 10);
+    // 円の中心座標を初期化
+    loc_x2 = ofRandom(0, ofGetWidth());
+    loc_y2 = ofRandom(0, ofGetHeight());
+    // 初速度を定義
+    speed_x2 = ofRandom(-20, 20);
+    speed_y2 = ofRandom(-20, 20);
+    // 円の中心座標を初期化
+    loc_x3 = ofRandom(0, ofGetWidth());
+    loc_y3 = ofRandom(0, ofGetHeight());
+    // 初速度を定義
+    speed_x3 = ofRandom(-30, 30);
+    speed_y3 = ofRandom(-30, 30);
 }
 
 //--------------------------------------------------------------
@@ -31,24 +51,64 @@ void ofApp::setup(){
 /* 更にプログラム的観点から書き換えると... */
 // 変数の値の更新
 void ofApp::update(){
-    loc_x = loc_x + speed_x;
-    loc_y = loc_y + speed_y;
+    loc_x1 = loc_x1 + speed_x1;
+    loc_y1 = loc_y1 + speed_y1;
 
     // 条件1: 左端で跳ね返る
-    if (loc_x < 0) {
-        speed_x = speed_x * -1;
+    if (loc_x1 < 0) {
+        speed_x1 = speed_x1 * -1;
     }
     // 条件2: 右端で跳ね返る
-    if (loc_x > ofGetWidth()) {
-        speed_x = speed_x * -1;
+    if (loc_x1 > ofGetWidth()) {
+        speed_x1 = speed_x1 * -1;
     }
     // 条件3: 上端で跳ね返る
-    if (loc_y < 0) {
-        speed_y = speed_y * -1;
+    if (loc_y1 < 0) {
+        speed_y1 = speed_y1 * -1;
     }
     // 条件4: 下端で跳ね返る
-    if (loc_y > ofGetHeight()) {
-        speed_y = speed_y * -1;
+    if (loc_y1 > ofGetHeight()) {
+        speed_y1 = speed_y1 * -1;
+    }
+    
+    loc_x2 = loc_x2 + speed_x2;
+    loc_y2 = loc_y2 + speed_y2;
+
+    // 条件1: 左端で跳ね返る
+    if (loc_x2 < 0) {
+        speed_x2 = speed_x2 * -1;
+    }
+    // 条件2: 右端で跳ね返る
+    if (loc_x2 > ofGetWidth()) {
+        speed_x2 = speed_x2 * -1;
+    }
+    // 条件3: 上端で跳ね返る
+    if (loc_y2 < 0) {
+        speed_y2 = speed_y2 * -1;
+    }
+    // 条件4: 下端で跳ね返る
+    if (loc_y2 > ofGetHeight()) {
+        speed_y2 = speed_y2 * -1;
+    }
+    
+    loc_x3 = loc_x3 + speed_x3;
+    loc_y3 = loc_y3 + speed_y3;
+
+    // 条件1: 左端で跳ね返る
+    if (loc_x3 < 0) {
+        speed_x3 = speed_x3 * -1;
+    }
+    // 条件2: 右端で跳ね返る
+    if (loc_x3 > ofGetWidth()) {
+        speed_x3 = speed_x3 * -1;
+    }
+    // 条件3: 上端で跳ね返る
+    if (loc_y3 < 0) {
+        speed_y3 = speed_y3 * -1;
+    }
+    // 条件4: 下端で跳ね返る
+    if (loc_y3 > ofGetHeight()) {
+        speed_y3 = speed_y3 * -1;
     }
 }
 
@@ -60,7 +120,9 @@ void ofApp::update(){
 // 描画
 void ofApp::draw(){
     ofSetColor(31, 63, 255); // 描画色設定
-    ofCircle(loc_x, loc_y, 40); // 円を描画する
+    ofCircle(loc_x1, loc_y1, 40); // 円を描画する
+    ofCircle(loc_x2, loc_y2, 40); // 円を描画する
+    ofCircle(loc_x3, loc_y3, 40); // 円を描画する
 }
 
 //--------------------------------------------------------------
