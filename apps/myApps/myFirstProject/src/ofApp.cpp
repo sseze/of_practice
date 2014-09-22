e47603c8317a9e59c31293945cb49f403e67befc
sseze <sseze.ek@gmail.com>
2014-09-22 11:35:50 +0900

    半透明な同心円を描く練習

--- apps/myApps/myFirstProject/src/ofApp.cpp
+++ apps/myApps/myFirstProject/src/ofApp.cpp
@@ -4,6 +4,7 @@
 void ofApp::setup(){
     ofBackground(31, 31, 31); // 背景色の設定
     ofEnableAlphaBlending(); // 透明度(アルファチャンネル)を有効にする
+    ofSetCircleResolution(64); // 円の解像度を設定
 }
 
 //--------------------------------------------------------------
@@ -13,17 +14,23 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-//    単純図形の描画と色設定
-    ofSetColor(242, 204, 242, 127); // 描画色の設定
-    ofLine(100, 300, 800, 300); // 線を描く
-    ofSetColor(242, 204, 47, 127); // 描画色の設定
-    ofRect(500, 250, 150, 200); // 四角形を描く
-    ofSetColor(174, 221, 60, 127); // 描画色の設定
-    ofCircle(400, 250, 150); // 真円を描く
-    ofSetColor(116, 193, 206, 127); // 描画色の設定
-    ofEllipse(400, 450, 300, 200); // 楕円を描く
-    ofSetColor(211, 24, 24, 127); // 描画色の設定
-    ofTriangle(420, 200, 200, 500, 550, 400); // 三角形を描く
+    // 同心円を描く
+    float x;
+    float y;
+    float radius;
+    x = 1024/2;
+    y = 768/2;
+    radius = 30;
+    radius = radius + 30;
+    ofSetColor(31, 127, 255, 127);
+    ofCircle(x, y, radius); // 円を描く
+    ofCircle(x, y, radius+40); // 半径を40増加させて円を描く
+    ofCircle(x, y, radius+80); // 半径を80増加させて円を描く
+    ofCircle(x, y, radius+120); // 半径を120増加させて円を描く
+    ofCircle(x, y, radius+160); // 半径を160増加させて円を描く
+    ofCircle(x, y, radius+200); // 半径を200増加させて円を描く
+    ofCircle(x, y, radius+240); // 半径を240増加させて円を描く
+
 }
 
 //--------------------------------------------------------------
