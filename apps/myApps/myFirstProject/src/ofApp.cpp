256f4194dcff81784a27e5ba88f90b5a711dd387
sseze <sseze.ek@gmail.com>
2014-09-22 11:55:35 +0900

    画面一杯にランダムにたくさんの円を描く練習

--- apps/myApps/myFirstProject/src/ofApp.cpp
+++ apps/myApps/myFirstProject/src/ofApp.cpp
@@ -1,9 +1,22 @@
 #include "ofApp.h"
 
+// 画面いっぱいにランダムにたくさんの円を描く
+float x[1000];
+float y[1000];
+float radius[1000];
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(31, 31, 31); // 背景色の設定
     ofEnableAlphaBlending(); // 透明度(アルファチャンネル)を有効にする
+    
+    // 画面いっぱいにランダムにたくさんの円を描く
+    int i;
+    for (i=0; i<1000; i++) {
+        x[i] = ofRandom(0, ofGetWidth()); // x座標は0~横幅一杯まで
+        y[i] = ofRandom(0, ofGetHeight()); // y座標は0~縦幅一杯まで
+        radius[i] = ofRandom(10, 40);
+    }
 }
 
 //--------------------------------------------------------------
@@ -13,21 +26,11 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    // グラデーションを作る
-    float w = ofGetWidth()/30.0 + 1;  // 長方形の幅を指定
-    float h = ofGetHeight()/20.0 + 1; // 長方形の高さを指定
-    float x = 0; // x座標を0に
-    float y = ofGetHeight()/2 - h/2; // y座標を画面の上下の中心に
-    float i; // 横用
-    float j; // 縦用
-    
-    for (j=0; j<20; j++) {
-        for(i=0; i<30; i++) {
-            ofSetColor(255/20 * j, 127, 255/30 * i, 127);
-            x = ofGetWidth() / 30.0 * i; // x座標上に等間隔に配置
-            y = ofGetHeight() / 20.0 * j; // y座標上に等間隔に配置
-            ofRect(x, y, w, h); // 長方形を描く
-        }
+    // 画面いっぱいにランダムにたくさんの円を描く
+    int i;
+    ofSetColor(31, 63, 255, 63); // 描画色を設定
+    for (i=0; i<1000; i++) {
+        ofCircle(x[i], y[i], radius[i]);
     }
 }
 
