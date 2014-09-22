d163fc7bf08c99cd19970909cafd24764c842003
sseze <sseze.ek@gmail.com>
2014-09-22 11:33:18 +0900

    first commit. - 単純図形の描画と色設定の練習

---
+++ libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofMaterial.d
@@ -0,0 +1,12 @@
+dependencies: \
+  /Users/sseze/Desktop/of_v0.8.4_osx_release/libs/openFrameworks/gl/ofMaterial.cpp \
+  ../../../../libs/openFrameworks/gl/ofMaterial.h \
+  /Users/sseze/Desktop/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofColor.h \
+  /Users/sseze/Desktop/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofMath.h \
+  /Users/sseze/Desktop/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofPoint.h \
+  /Users/sseze/Desktop/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofVec3f.h \
+  ../../../../libs/openFrameworks/math/ofVec2f.h \
+  /Users/sseze/Desktop/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
+  ../../../../libs/glew/include/GL/glew.h \
+  ../../../../libs/tess2/include/tesselator.h \
+  ../../../../libs/openFrameworks/math/ofVec4f.h
