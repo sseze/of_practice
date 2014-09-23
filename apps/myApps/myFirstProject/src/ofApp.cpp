13dfb04719a9592503e27c5590ecafcc5e1a1ea5
sseze <sseze.ek@gmail.com>
2014-09-24 02:06:28 +0900

    1000個のランダムな円を、マウスのクリックで集めたり放したりする

--- apps/myApps/myFirstProject/src/ofApp.cpp
+++ apps/myApps/myFirstProject/src/ofApp.cpp
@@ -1,7 +1,16 @@
 #include "ofApp.h"
 
-float loc_x, loc_y;
-int red, green, blue;
+static const int NUM = 1000;
+
+float loc_x[NUM];
+float loc_y[NUM];
+float speed_x[NUM];
+float speed_y[NUM];
+float radius[NUM];
+float red[NUM];
+float green[NUM];
+float blue[NUM];
+bool mouse_pressed; // マウスがクリックされたか？
 
 //--------------------------------------------------------------
 // 初期化関数: プログラム実行時に1度だけ実行される
@@ -13,15 +22,20 @@ void ofApp::setup(){
     ofBackground(0, 0, 0);
     ofSetFrameRate(60); // フレームレート
     ofSetCircleResolution(64); // 解像度
-   
-    // 円の初期位置
-    loc_x = ofGetWidth() / 2;
-    loc_y = ofGetHeight() / 2;
+    ofEnableAlphaBlending(); // アルファチャンネル(透過)を有効にする
     
-    // 円の初期色
-    red = 31;
-    green = 63;
-    blue = 255;
+    mouse_pressed = false; // マウスがクリックされていない状態で初期化
+    
+    for (int i=0; i<NUM; i++) {
+        loc_x[i] = ofRandom(0, ofGetWidth());
+        loc_y[i] = ofRandom(0, ofGetHeight());
+        speed_x[i] = ofRandom(-5, 5);
+        speed_y[i] = ofRandom(-5, 5);
+        radius[i] = ofRandom(4, 40);
+        red[i] = ofRandom(0, 255);
+        green[i] = ofRandom(0, 255);
+        blue[i] = ofRandom(0, 255);
+    }
 }
 
 //--------------------------------------------------------------
@@ -31,6 +45,35 @@ void ofApp::setup(){
 /* 更にプログラム的観点から書き換えると... */
 // 変数の値の更新
 void ofApp::update(){
+    for (int i=0; i<NUM; i++) {
+        // もしマウスがクリックされていたら円がマウスに集まってくる
+        if (mouse_pressed) {
+            // マウスの現在位置から円のスピードを再計算
+            // マウスのx座標と円のx座標の距離の1/8だけ接近
+            speed_x[i] = (mouseX - loc_x[i]) / 8.0;
+            // マウスのy座標と円のy座標の距離の1/8だけ接近
+            speed_y[i] = (mouseY - loc_y[i]) / 8.0;
+        }
+        
+        // 円の座標を更新
+        loc_x[i] = loc_x[i] + speed_x[i];
+        loc_y[i] = loc_y[i] + speed_y[i];
+        
+        // 跳ね返り条件
+        if (loc_x[i] < 0) {
+            speed_x[i] = speed_x[i] * -1;
+        }
+        if (loc_x[i] > ofGetWidth()) {
+            speed_x[i] = speed_x[i] * -1;
+        }
+        if (loc_y[i] < 0) {
+            speed_y[i] = speed_y[i] * -1;
+        }
+        if (loc_y[i] > ofGetHeight()) {
+            speed_y[i] = speed_y[i] * -1;
+        }
+    }
+    
 }
 
 //--------------------------------------------------------------
@@ -40,8 +83,10 @@ void ofApp::update(){
 /* 更にプログラム的観点から書き換えると... */
 // 描画
 void ofApp::draw(){
-    ofSetColor(red, green, blue); // 描画色
-    ofCircle(loc_x, loc_y, 40); // マウスの現在位置を中心に円を描く
+    for (int i=0; i<NUM; i++) {
+        ofSetColor(red[i], green[i], blue[i], 127);
+        ofCircle(loc_x[i], loc_y[i], radius[i]);
+    }
 }
 
 //--------------------------------------------------------------
@@ -58,39 +103,29 @@ void ofApp::keyReleased(int key){
 //--------------------------------------------------------------
 // マウスを移動したときを検知して実行する
 void ofApp::mouseMoved(int x, int y ){
-    red = 127;
-    green = 127;
-    blue = 127;
 }
 
 //--------------------------------------------------------------
 // マウスをドラッグしたときを検知して実行する
 void ofApp::mouseDragged(int x, int y, int button){
-    // 円の中心位置をマウスの位置に
-    loc_x = x;
-    loc_y = y;
 }
 
 //--------------------------------------------------------------
 // マウスをクリックしたときを検知して実行する
 void ofApp::mousePressed(int x, int y, int button){
-    // 円の中心位置をマウスの位置に
-    loc_x = x;
-    loc_y = y;
-    
-    // 円の色を赤に
-    red = 255;
-    green = 63;
-    blue = 31;
+    mouse_pressed = true; // マウスが押されている状態に
 }
 
 //--------------------------------------------------------------
 // クリックされていたマウスのボタンがリリースされたときに実行する
 void ofApp::mouseReleased(int x, int y, int button){
-    // 円の色を青に
-    red = 31;
-    green = 63;
-    blue = 255;
+    mouse_pressed = false; // マウスが押されていない状態に
+    
+    // 円のスピードを再初期化
+    for (int i=0; i<NUM; i++) {
+        speed_x[i] = ofRandom(-5, 5);
+        speed_y[i] = ofRandom(-5, 5);
+    }
 }
 
 //--------------------------------------------------------------
